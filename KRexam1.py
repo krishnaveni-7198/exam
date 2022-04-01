adebe5a8236d3e82ff5374cd211af4a5a1420062
krishnaveni-7198 <74653716+krishnaveni-7198@users.noreply.github.com>
2022-04-01 12:07:16 +0530

    Add files via upload

---
+++ KRexam1.py
@@ -0,0 +1,17 @@
+import numpy as np
+M1=np.array([[4,5],[3,4]])
+M2=np.array([[4,6],[3,2]])
+print("First 2D array")
+print(M1)
+print("Second 2D array is:")
+print(M2)
+print("The transpose of a matrix is:")
+M3=M2.transpose()
+print(M3)
+print("The multiplication of this two matrises")
+M4=M1*M2
+print(M4)
+
+
+
+
